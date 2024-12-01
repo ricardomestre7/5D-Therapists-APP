f14a60dafe5e307320f1c44e34d55bd0d7cabede
Mestre Ricardo <98619234+ricardomestre7@users.noreply.github.com>
2024-12-01 21:42:11 +0000

    Inicialização do plugin 5D Therapists

---
+++ plugins/5d-therapists-plugin/5d-therapists-plugin.php/plugins/5d-therapists-plugin/5d-therapists-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: 5D Therapists Plugin
+Description: Plugin personalizado para gerenciar o app 5D Therapists no WordPress, incluindo login, cadastro e dashboard.
+Version: 1.0
+Author: Mestre Ricardo
+*/
+
+// Evitar acesso direto
+if (!defined('ABSPATH')) {
+    exit;
+}
